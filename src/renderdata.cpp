154508735616c98a6b4e0a389c4656903081a4ad
giuliom95 <giuliomartella1995@gmail.com>
2020-06-28 16:50:31 +0200

    Store all path method

--- src/renderdata.cpp
+++ src/renderdata.cpp
@@ -14,12 +14,16 @@ void RenderData::disk_load_all()
 	{
 		// Number of points in path 
 		uint8_t npoints;
-		filestream.read((char*)&npoints, sizeof(uint8_t));
+		filestream.read
+		(
+			reinterpret_cast<char*>(&npoints), 
+			sizeof(uint8_t)
+		);
 
 		Path path(npoints);
 		filestream.read
 		(
-			(char*)path.points.data(), 
+			reinterpret_cast<char*>(path.points.data()), 
 			npoints*sizeof(Vec3h)
 		);
 		
@@ -33,5 +37,20 @@ void RenderData::disk_load_all()
 
 void RenderData::disk_store_all()
 {
-	
+	std::ofstream filestream{filepath.string()};
+
+	for(const Path& path : paths)
+	{
+		uint8_t npoints = (uint8_t)path.points.size();
+		filestream.put(npoints);
+		filestream.write
+		(
+			reinterpret_cast<const char*>(path.points.data()),
+			npoints*sizeof(Vec3h)
+		);
+	}
+
+	filestream.close();
+
+	BOOST_LOG_TRIVIAL(info) << "Wrote " << paths.size() << " paths";
 }
