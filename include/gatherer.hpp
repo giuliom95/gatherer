ddee9ba22a673354a9a406c76c7d7644640713e7
giuliom95 <giuliomartella1995@gmail.com>
2020-07-06 11:12:54 +0200

    Made RenderData template based

--- include/gatherer.hpp
+++ include/gatherer.hpp
@@ -6,27 +6,73 @@
 #include <boost/filesystem.hpp>
 #include <boost/log/trivial.hpp>
 
-using Vec3 = Vec3f;
-
+template <class Vec3>
 class Path
 {
 public:
-	Path();
-	Path(const size_t npoints);
-	void add_point(const Vec3& p);
+	Path() : Path(0) {}
+	Path(const size_t npoints)
+	{
+		points = std::vector<Vec3>(npoints);
+	}
+
+	void add_point(const Vec3& p)
+	{
+		points.push_back(p);
+	}
+	
 	std::vector<Vec3> points;
 };
 
-using PathsGroup = std::vector<Path>;
+template <class Vec3>
+using PathsGroup = std::vector<Path<Vec3>>;
 
+template <class Vec3>
 class RenderData
 {
 public:
-	RenderData(const unsigned nthreads=1);
-	void disk_load_all (const boost::filesystem::path& dirpath);
-	void disk_store_all(const boost::filesystem::path& dirpath);
+	RenderData(const unsigned nthreads=1)
+	{
+		pathgroups = std::vector<PathsGroup<Vec3>>(nthreads);
+		for(unsigned pgi = 0; pgi < nthreads; ++pgi)
+			pathgroups[pgi] = PathsGroup<Vec3>();
+	}
 
-	std::vector<PathsGroup>	pathgroups;
+	//void disk_load_all (const boost::filesystem::path& dirpath);
+	
+	void disk_store_all(
+		const boost::filesystem::path& dirpath
+	) {
+		if(!boost::filesystem::create_directory(dirpath))
+		{
+			BOOST_LOG_TRIVIAL(warning) << "Directory already there, overwriting";
+		}
+		boost::filesystem::ofstream lenghts_ofs{dirpath / "lenghts.bin"};
+		boost::filesystem::ofstream paths_ofs{dirpath / "paths.bin"}; 
+		
+		unsigned long npaths = 0;
+
+		for(const PathsGroup<Vec3> pg : pathgroups)
+		{
+			for(const Path<Vec3>& path : pg)
+			{
+				++npaths;
+				const uint8_t npoints = (uint8_t)path.points.size();
+				lenghts_ofs.write(reinterpret_cast<const char*>(&npoints), 1);
+				paths_ofs.write
+				(
+					reinterpret_cast<const char*>(path.points.data()),
+					npoints*sizeof(Vec3)
+				);
+			}
+		}
+		lenghts_ofs.close();
+		paths_ofs.close();
+
+		BOOST_LOG_TRIVIAL(info) << "Wrote " << npaths << " paths";
+	}
+
+	std::vector<PathsGroup<Vec3>> pathgroups;
 };
 
 #endif
