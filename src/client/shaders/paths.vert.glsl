9a01cfb18a02aa9d0b53f4812e7aa58b0c941b36
giuliom95 <giuliomartella1995@gmail.com>
2020-07-30 00:21:08 +0200

    Changed aPos into vtx_in

--- src/client/shaders/paths.vert.glsl
+++ src/client/shaders/paths.vert.glsl
@@ -1,5 +1,5 @@
 #version 450 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 vtx_in;
 
 uniform mat4 vpmat;
 
@@ -11,6 +11,6 @@ void main()
 	//	-5.35714943625e-16,   0.0,            -1.00001990795,      1.0, 
 	//	 1216.09556675,      -1194.22334433,  -1186.37361909,      1186.54999929
 	//);
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0)*vpmat;
+	gl_Position = vec4(vtx_in.x, vtx_in.y, vtx_in.z, 1.0)*vpmat;
 	gl_Position.w += .42;
 }
