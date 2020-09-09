08992a3dfb0c9e15f2b2b97715aeec5389ff2bba
giuliom95 <giuliomartella1995@gmail.com>
2020-09-09 20:22:29 +0200

    Gatherer reworking

--- include/gatherer.hpp
+++ include/gatherer.hpp
@@ -7,6 +7,144 @@
 #include <boost/filesystem.hpp>
 #include <boost/log/trivial.hpp>
 
+class CameraSample
+{
+public:
+	uint16_t i, j;
+	half u, v;
+};
+
+template<typename T>
+class Buffer
+{
+public:
+	std::vector<T> data;
+	boost::filesystem::ofstream outstream;
+
+	void storechunk()
+	{
+		outstream.write(
+			reinterpret_cast<char*>(data.data()),
+			data.size() * sizeof(T)
+		);
+		data.clear();
+	}
+};
+
+class DataBlock
+{
+public:
+	static constexpr size_t maxpaths = 128*64*64;
+	static constexpr size_t maxbounces = 10;
+	static constexpr size_t maxgeom = maxpaths*maxbounces;
+
+	Buffer<Vec3h>			bouncespositions;
+
+	Buffer<Vec3h>			luminance;
+	Buffer<uint8_t>			lenghts;
+	Buffer<CameraSample>	camerasamples;
+
+	void reserve()
+	{
+		bouncespositions.data.reserve(maxgeom);
+		luminance.data.reserve(maxpaths);
+		lenghts.data.reserve(maxpaths);
+		camerasamples.data.reserve(maxpaths);
+	}
+
+	void openfiles(unsigned ti, const boost::filesystem::path& folder)
+	{
+		char filename[20];
+		std::sprintf(filename, "positions%02d.bin", ti);
+		const boost::filesystem::path bppath = folder / "bounces" / filename;
+		BOOST_LOG_TRIVIAL(info) << bppath;
+		bouncespositions.outstream.open(bppath);
+
+		std::sprintf(filename, "luminance%02d.bin", ti);
+		const boost::filesystem::path lumpath = folder / "paths" / filename;
+		BOOST_LOG_TRIVIAL(info) << lumpath;
+		luminance.outstream.open(lumpath);
+
+		std::sprintf(filename, "lenghts%02d.bin", ti);
+		const boost::filesystem::path lenpath = folder / "paths" / filename;
+		BOOST_LOG_TRIVIAL(info) << lenpath;
+		lenghts.outstream.open(lenpath);
+
+		std::sprintf(filename, "camerasamples%02d.bin", ti);
+		const boost::filesystem::path cspath = folder / "paths" / filename;
+		BOOST_LOG_TRIVIAL(info) << cspath;
+		camerasamples.outstream.open(cspath);
+	}
+
+	void closefiles()
+	{
+		bouncespositions.outstream.close();
+		luminance.outstream.close();
+		lenghts.outstream.close();
+		camerasamples.outstream.close();
+	}
+};
+
+
+class Gatherer
+{
+public:
+	Gatherer(unsigned nthreads, const boost::filesystem::path& folder)
+	{
+		BOOST_LOG_TRIVIAL(info) << "Initializing Gatherer";
+		data = std::vector<DataBlock>(nthreads);
+
+		for(unsigned ti = 0; ti < nthreads; ++ti)
+		{
+			DataBlock& db = data[ti];
+			db.reserve();
+			db.openfiles(ti, folder);
+		}
+	}
+
+	~Gatherer()
+	{
+		BOOST_LOG_TRIVIAL(info) << "Releasing Gatherer";
+		for(DataBlock& db : data)
+		{
+			db.bouncespositions.storechunk();
+			db.lenghts.storechunk();
+			db.luminance.storechunk();
+			db.camerasamples.storechunk();
+			db.closefiles();
+		}
+	}
+
+	void addbounce(unsigned tid, Vec3h pos)
+	{
+		std::vector<Vec3h>& pd = data[tid].bouncespositions.data;
+		pd.push_back(pos);
+		if(pd.size() >= pd.capacity())
+		{
+			data[tid].bouncespositions.storechunk();
+		}
+	}
+
+	void finalizepath(unsigned tid, Vec3h luminance, CameraSample sample)
+	{
+		DataBlock& db = data[tid];
+		db.lenghts.data.push_back(0);
+		db.luminance.data.push_back(luminance);
+		db.camerasamples.data.push_back(sample);
+
+		std::vector<uint8_t>& ld = db.lenghts.data;
+		if(ld.size() >= ld.capacity())
+		{
+			data[tid].lenghts.storechunk();
+			data[tid].luminance.storechunk();
+			data[tid].camerasamples.storechunk();
+		}
+	}
+
+private:
+	std::vector<DataBlock> data;
+};
+/*
 class Path
 {
 public:
@@ -104,5 +242,5 @@ public:
 
 	std::vector<PathsGroup> pathgroups;
 };
-
+*/
 #endif
