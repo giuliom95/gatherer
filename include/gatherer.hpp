b3f1482eb5cd00622a036614bdcf72b961fe1d14
giuliom95 <giuliomartella1995@gmail.com>
2020-09-10 12:41:26 +0200

    Multithread files compacted into one

--- include/gatherer.hpp
+++ include/gatherer.hpp
@@ -19,7 +19,9 @@ class Buffer
 {
 public:
 	std::vector<T> data;
-	boost::filesystem::ofstream outstream;
+	boost::filesystem::ofstream	outstream;
+	boost::filesystem::path		outfile;
+
 
 	void storechunk()
 	{
@@ -56,24 +58,20 @@ public:
 	{
 		char filename[20];
 		std::sprintf(filename, "positions%02d.bin", ti);
-		const boost::filesystem::path bppath = folder / "bounces" / filename;
-		BOOST_LOG_TRIVIAL(info) << bppath;
-		bouncespositions.outstream.open(bppath);
+		bouncespositions.outfile = folder / "bounces" / filename;
+		bouncespositions.outstream.open(bouncespositions.outfile);
 
 		std::sprintf(filename, "luminance%02d.bin", ti);
-		const boost::filesystem::path lumpath = folder / "paths" / filename;
-		BOOST_LOG_TRIVIAL(info) << lumpath;
-		luminance.outstream.open(lumpath);
+		luminance.outfile = folder / "paths" / filename;
+		luminance.outstream.open(luminance.outfile);
 
 		std::sprintf(filename, "lenghts%02d.bin", ti);
-		const boost::filesystem::path lenpath = folder / "paths" / filename;
-		BOOST_LOG_TRIVIAL(info) << lenpath;
-		lenghts.outstream.open(lenpath);
+		lenghts.outfile = folder / "paths" / filename;
+		lenghts.outstream.open(lenghts.outfile);
 
 		std::sprintf(filename, "camerasamples%02d.bin", ti);
-		const boost::filesystem::path cspath = folder / "paths" / filename;
-		BOOST_LOG_TRIVIAL(info) << cspath;
-		camerasamples.outstream.open(cspath);
+		camerasamples.outfile = folder / "paths" / filename;
+		camerasamples.outstream.open(camerasamples.outfile);
 	}
 
 	void closefiles()
@@ -93,6 +91,7 @@ public:
 	{
 		BOOST_LOG_TRIVIAL(info) << "Initializing Gatherer";
 		data = std::vector<DataBlock>(nthreads);
+		folderpath = folder;
 
 		for(unsigned ti = 0; ti < nthreads; ++ti)
 		{
@@ -105,14 +104,46 @@ public:
 	~Gatherer()
 	{
 		BOOST_LOG_TRIVIAL(info) << "Releasing Gatherer";
+
+		boost::filesystem::ofstream bpos_ofs{
+			folderpath / "bounces" / "positions.bin"
+		};
+		boost::filesystem::ofstream lum_ofs{
+			folderpath / "paths" / "luminance.bin"
+		};
+		boost::filesystem::ofstream len_ofs{
+			folderpath / "paths" / "lenghts.bin"
+		};
+		boost::filesystem::ofstream cs_ofs{
+			folderpath / "paths" / "camerasamples.bin"
+		};
+
 		for(DataBlock& db : data)
 		{
 			db.bouncespositions.storechunk();
-			db.lenghts.storechunk();
 			db.luminance.storechunk();
+			db.lenghts.storechunk();
 			db.camerasamples.storechunk();
+
 			db.closefiles();
+
+			std::vector<char> buf(1024);
+
+			movefilecontents(db.bouncespositions.outfile, buf, bpos_ofs);
+			movefilecontents(db.luminance.outfile, buf, lum_ofs);
+			movefilecontents(db.lenghts.outfile, buf, len_ofs);
+			movefilecontents(db.camerasamples.outfile, buf, cs_ofs);
+
+			boost::filesystem::remove(db.bouncespositions.outfile);
+			boost::filesystem::remove(db.luminance.outfile);
+			boost::filesystem::remove(db.lenghts.outfile);
+			boost::filesystem::remove(db.camerasamples.outfile);
 		}
+
+		bpos_ofs.close();
+		lum_ofs.close();
+		len_ofs.close();
+		cs_ofs.close();
 	}
 
 	void addbounce(unsigned tid, Vec3h pos)
@@ -142,105 +173,23 @@ public:
 	}
 
 private:
-	std::vector<DataBlock> data;
-};
-/*
-class Path
-{
-public:
-	Path() : Path(0) {}
-	Path(const size_t npoints)
-	{
-		points = std::vector<Vec3h>(npoints);
-	}
+	std::vector<DataBlock>	data;
+	boost::filesystem::path	folderpath;
 
-	void add_point(const Vec3h& p)
-	{
-		points.push_back(p);
-	}
-	
-	std::vector<Vec3h> points;
-};
-
-using PathsGroup = std::vector<Path>;
-
-
-class RenderData
-{
-public:
-	RenderData(const unsigned nthreads=1)
-	{
-		pathgroups = std::vector<PathsGroup>(nthreads);
-		for(unsigned pgi = 0; pgi < nthreads; ++pgi)
-			pathgroups[pgi] = PathsGroup();
-	}
-
-	void disk_load_all (
-		const boost::filesystem::path& dirpath
+	void movefilecontents(
+		boost::filesystem::path& ifs_path,
+		std::vector<char>& buf,
+		boost::filesystem::ofstream& ofs
 	) {
-		const boost::filesystem::path lengths_fp = dirpath / "lengths.bin";
-		const boost::filesystem::path paths_fp   = dirpath / "paths.bin";
-		boost::filesystem::ifstream lengths_ifs(lengths_fp);
-		boost::filesystem::ifstream paths_ifs  (paths_fp);
-
-		pathgroups = std::vector<PathsGroup>(1);
-		pathgroups[0] = PathsGroup();
-
-		while(!lengths_ifs.eof())
+		boost::filesystem::ifstream ifs{ifs_path};
+		while(!ifs.eof())
 		{
-			// Number of points in path 
-			uint8_t npoints;
-			lengths_ifs.read((char*)&npoints, sizeof(uint8_t));
-
-			Path path(npoints);
-			paths_ifs.read
-			(
-				(char*)path.points.data(), 
-				npoints*sizeof(Vec3h)
-			);
-
-			pathgroups[0].push_back(path);
+			ifs.read(buf.data(), buf.size());
+			std::streamsize readbytes = ifs.gcount();
+			ofs.write(buf.data(), readbytes);
 		}
-		lengths_ifs.close();
-		paths_ifs.close();
-
-		BOOST_LOG_TRIVIAL(info) << "Loaded " << pathgroups[0].size() << " paths";
+		ifs.close();
 	}
-
-	
-	void disk_store_all(
-		const boost::filesystem::path& dirpath
-	) {
-		if(!boost::filesystem::create_directory(dirpath))
-		{
-			BOOST_LOG_TRIVIAL(warning) << "Directory already there, overwriting";
-		}
-		boost::filesystem::ofstream lenghts_ofs{dirpath / "lengths.bin"};
-		boost::filesystem::ofstream paths_ofs{dirpath / "paths.bin"}; 
-		
-		unsigned long npaths = 0;
-
-		for(const PathsGroup pg : pathgroups)
-		{
-			for(const Path& path : pg)
-			{
-				++npaths;
-				const uint8_t npoints = (uint8_t)path.points.size();
-				lenghts_ofs.write(reinterpret_cast<const char*>(&npoints), 1);
-				paths_ofs.write
-				(
-					reinterpret_cast<const char*>(path.points.data()),
-					npoints*sizeof(Vec3h)
-				);
-			}
-		}
-		lenghts_ofs.close();
-		paths_ofs.close();
-
-		BOOST_LOG_TRIVIAL(info) << "Wrote " << npaths << " paths";
-	}
-
-	std::vector<PathsGroup> pathgroups;
 };
-*/
+
 #endif
