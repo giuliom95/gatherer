fc41bc22fefaf9fec224e905aa0a171a5e61732a
giuliom95 <giuliomartella1995@gmail.com>
2020-10-11 20:59:53 +0200

    Half space window selector

--- src/client/windowfilter.cpp
+++ src/client/windowfilter.cpp
@@ -59,8 +59,59 @@ void WindowFilter::render(
 void WindowFilter::computepaths(GatheredData& gd)
 {
 	// Do nothing for now
-	auto npaths = gd.selectedpaths.size();
-	LOG(info) << "Filtered paths (" << npaths << " / " << npaths << ")";
+	const unsigned npaths = gd.selectedpaths.size();
+
+	std::vector<unsigned> selectedpaths;
+	selectedpaths.reserve(npaths);
+
+	for(unsigned i = 0; i < npaths; ++i)
+	{
+		const unsigned path_i = gd.selectedpaths[i];
+		const unsigned nbounces = gd.pathslength[path_i];
+		const unsigned off = gd.firstbounceindexes[path_i];
+
+		// Get first bounce
+		Vec3f prev_b = fromVec3h(gd.bouncesposition[off]);
+		// Transform it
+		Vec3f t_prev_b = transformPoint(w2o, prev_b);
+
+		LOG(info) << t_prev_b;
+
+		//Starts from the second one
+		for(unsigned b_i = off+1; b_i < off + nbounces; b_i++)
+		{
+			Vec3f this_b = fromVec3h(gd.bouncesposition[b_i]);
+			Vec3f t_this_b = transformPoint(w2o, this_b);
+
+			LOG(info) << t_this_b;
+
+			// On the opposite half spaces
+			if(t_this_b[0] * t_prev_b[0] < 1)
+			{
+				selectedpaths.push_back(path_i);
+				LOG(info) << "FOUND";
+				break;
+			}
+
+			prev_b = this_b;
+			t_prev_b = t_this_b;
+		}
+	}
+
+	unsigned newnpaths = selectedpaths.size();
+
+	std::move(
+		selectedpaths.begin(), 
+		selectedpaths.end(), 
+		gd.selectedpaths.begin()
+	);
+
+	gd.selectedpaths.erase(
+		gd.selectedpaths.begin() + newnpaths, 
+		gd.selectedpaths.end()
+	);
+
+	LOG(info) << "Filtered paths (" << newnpaths << " / " << npaths << ")";
 }
 
 bool WindowFilter::renderstackui()
@@ -70,9 +121,12 @@ bool WindowFilter::renderstackui()
 	modified |= ImGui::DragFloat3(
 		"Position", reinterpret_cast<float*>(&position)
 	);
-	modified |= ImGui::DragFloat3(
-		"Normal", reinterpret_cast<float*>(&normal)
-	);
+	if(ImGui::DragFloat3(
+		"Normal", reinterpret_cast<float*>(&normal), 0.01f
+	)) {
+		normal = normalize(normal);
+		modified = true;
+	}
 	modified |= ImGui::DragFloat2(
 		"Size", reinterpret_cast<float*>(&size)
 	);
@@ -86,6 +140,6 @@ void WindowFilter::updatematrices()
 {
 	const Mat4f axes(refFromVec(normal));
 	const Mat4f axes_t(transpose(axes));
-	o2w = axes   * translationMatrix(   position);
-	w2o = axes_t * translationMatrix(-1*position);
+	o2w = axes * translationMatrix(position);
+	w2o = translationMatrix(-1*position) * axes_t;
 }
